41855ee05b1b850c818eb55d693c53bca59081a7
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2016-01-13 13:20:41 +0000

    Revert "renommage modif structure"
    This reverts commit a10c6c4b0cb60168cada129702982f66170da00e.

--- inc/data_mem.h
+++ inc/data_mem.h
@@ -18,8 +18,8 @@ struct data_mem
 };
 
 
-void store(int pos, struct data_mem *dm, char *var_name, int val);
-int get_mem_location(char *var_name, struct data_mem *dm);			// Returns the index where var_name is stored
+void store(int pos,struct data_mem *dm,char*var_name,int val);
+int get_mem_location(char*var_name,struct data_mem *dm);			// Returns the index where var_name is stored
 		
 
 #endif		//End of file
