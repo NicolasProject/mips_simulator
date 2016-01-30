ec1bbe44bfbeed26eb7864592750c79fc8c2014f
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2016-01-30 17:18:43 +0000

    commentaire registers et quelque modif de code

--- inc/registers.h
+++ inc/registers.h
@@ -1,7 +1,6 @@
 #ifndef REGISTERS_H_INCLUDED
 #define REGISTERS_H_INCLUDED
 
-/* File to declare the struct reg ; and delcare the functions that are used to manipulate registers. */
 
 #include <stdio.h>
 #include <string.h>
@@ -10,20 +9,20 @@
 
 #define REG_NAME_SIZE 5
 
+// definition of a register
 struct reg
 {
-	char alt_name[REG_NAME_SIZE];			// Stores names like t0, fp, at...
-	int val;								// Stores the value of the register
+	char alt_name[REG_NAME_SIZE];			// Register name
+	int val;								// Register value
 };
 
-
-extern struct reg reg_file[32];
-
-
-void init_reg_file();	   		// To initialise the register file (This is has to be called before the first reference to registers)
-int reg_num(char*alt_name);		// Returns the number of the register given the alternate name.
-void afficher_registres();		// Fonction qui affiche le contenu et le nom de tous les registres
+// definition in registers.c
+struct reg reg_file[32];
 
 
-#endif 	// End of file
+void init_reg_file();	   		// Initialize registers, must be called at the begining of the programm
+int reg_num(char*alt_name);		// Returns the number of the register
+void afficher_registres();		// Display content and name of register
 
+
+#endif
