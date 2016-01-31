62928b41691579b78c462baf5996878cda7b687f
tanguy2508 <tanguy.roberjot@hotmail.fr>
2016-01-31 18:08:00 +0000

    modifs mineures

--- inc/data_mem.h
+++ inc/data_mem.h
@@ -7,19 +7,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// data memory size
-#define DATA_MEM_SIZE 1024
-
-struct data_mem
-{
-	struct data_mem_element
-	{
-		char var_name[20];
-		int val;
+	// data memory size
+	#define DATA_MEM_SIZE 1024
 	
-	}mem[DATA_MEM_SIZE];
-};
+	struct data_mem
+	{
+		struct data_mem_element
+		{
+			char var_name[20];
+			int val;
 		
+		}mem[DATA_MEM_SIZE];
+	};
+			
 
 #endif		//End of file
 
